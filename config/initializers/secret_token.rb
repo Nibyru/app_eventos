73b9d5578a6e0dcf864e84bc19780d38f2bf499a
Sergio Sebastian Arriagada <sergio@hashdog.com>
2014-07-06 15:11:45 -0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AppEventos::Application.config.secret_key_base = '990616b93485ea62500ff18b40fcf9507f9d1431ae51079ebf9770dd9e6bd2aabc4cf76a55922e9c629df886cd6324d61f2c3efb4ed3c923eea2458c1b3305ae'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AppEventos::Application.config.session_store :cookie_store, key: '_app_eventos_session'
